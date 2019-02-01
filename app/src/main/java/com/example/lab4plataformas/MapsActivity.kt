795306de69e20192c883bfe2e24927df0ab9a68e
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2019-01-31 23:45:04 -0600

    Cambios

--- app/src/main/java/com/example/lab4plataformas/MapsActivity.kt
+++ app/src/main/java/com/example/lab4plataformas/MapsActivity.kt
@@ -36,10 +36,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Se agrega la latitud y longitud de guatemala
+        val guatemala = LatLng(14.6229, -90.5315)
+        mMap.addMarker(MarkerOptions().position(guatemala).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(guatemala))
     }
 
 
